b225a2af9705122b44ff62df893abe033540bf06
Zuhairah Muhamad <23ftt1096@student.pb.edu.bn>
2025-03-21 07:07:40 +0000

    Mail Logo Fix 5

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ config('http://asg2.duckdns.org') }}/pblogo.png" alt="PB Logo">
+<img src="{{ asset('pblogo.png') }}" alt="PB Logo">
 </a>
 </td>
 </tr>
