161a0cc63174c09ffbf8cf543a7982bf5cf1a271
Zuhairah Muhamad <23ftt1096@student.pb.edu.bn>
2025-03-21 08:20:27 +0000

    Mail Logo fix 6

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,12 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('pblogo.png') }}" alt="PB Logo">
+@if (trim($slot) === 'Laravel')
+<img src="http://asg2.duckdns.org/pblogo.png" class="logo" alt "pblogo">
+@else
+{{ $slot }}
+@endif
 </a>
 </td>
 </tr>
+
